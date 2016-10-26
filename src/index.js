b6631328c7b92664742553b4aef17df51d4fbd21
Eamonn <eamonn.m.r@gmail.com>
2016-10-25 20:57:38 -0400

    Parse CSV with promise-styled CSV library

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import csv from 'neat-csv'
 
-fetch('/mock_data.json')
-  .then((response) => {
-    response.json().then((data)=> {
-      ReactDOM.render(
-        <App trains={data}/>,
-        document.getElementById('root')
-      );
-    });
-  });
+function render_from_csv(){
+  fetch('/api/Departures.csv').then((response) => {
+    response.text().then( text => {
+	    csv( text ).then( data => {
+        console.log(data);
+        // ReactDOM.render(
+        //   <App trains={data}/>,
+        //   document.getElementById('root')
+        // );
+
+      })
+    })
+  })
+  
+  // TODO: sleep 30000 ms; render_from_csv()
+}
+
+render_from_csv();
 
